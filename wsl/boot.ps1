f91564b8dfb537d634b73710643fe7d5f703ff44
conjLob <53427774+conjLob@users.noreply.github.com>
2022-08-21 15:20:52 +0900

    Add WSL boot script

---
+++ wsl/boot.ps1
@@ -0,0 +1,14 @@
+#Requires -Version 6.0
+
+[System.Console]::OutputEncoding = [Text.Encoding]::Unicode
+
+$err = wsl -d ArchLinux --mount --vhd "$Env:LOCALAPPDATA\ArchLinux\home.vhdx" --bare
+
+if ($?) {
+    echo 'Your VHD was mounted successfully!'
+} elseif ($err[1] -ne 'Error code: Wsl/Service/AttachDisk/WSL_E_USER_VHD_ALREADY_ATTACHED') {
+    echo $err
+    exit 1
+}
+
+wsl -d ArchLinux --cd ~
