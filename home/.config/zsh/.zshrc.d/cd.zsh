c5abc4e60fc49c57c1697d25f01a9f4449cd2e8f
conjLob <53427774+conjLob@users.noreply.github.com>
2022-08-24 11:35:57 +0900

    Update zsh and prompt configs

--- home/.config/zsh/.zshrc.d/cd.zsh
+++ home/.config/zsh/.zshrc.d/cd.zsh
@@ -1,3 +1,5 @@
 setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushd_minus
 
 DIRSTACKSIZE=10
--- home/.config/zsh/.zshrc.d/history.zsh
+++ home/.config/zsh/.zshrc.d/history.zsh
@@ -1,9 +1,10 @@
 setopt share_history
 setopt extended_history
+setopt hist_reduce_blanks
+setopt hist_ignore_space
 setopt hist_ignore_dups
 setopt hist_expire_dups_first
 setopt hist_find_no_dups
-setopt hist_reduce_blanks
 
 HISTSIZE=100000
 SAVEHIST=10000
