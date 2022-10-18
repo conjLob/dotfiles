f42990b308c59e6890090f46b8c944836ccb9291
conjLob <53427774+conjLob@users.noreply.github.com>
2022-10-18 10:15:32 +0900

    Deal with changing the mount error code

--- wsl/boot.ps1
+++ wsl/boot.ps1
@@ -6,7 +6,7 @@ $err = wsl -d ArchLinux --mount --vhd "$Env:LOCALAPPDATA\ArchLinux\home.vhdx" --
 
 if ($?) {
     echo 'Your VHD was mounted successfully!'
-} elseif ($err[1] -ne 'Error code: Wsl/Service/AttachDisk/WSL_E_USER_VHD_ALREADY_ATTACHED') {
+} elseif ($err[1] -ne 'Error code: Wsl/Service/AttachDisk/MountVhd/WSL_E_USER_VHD_ALREADY_ATTACHED') {
     echo $err
     exit 1
 }
