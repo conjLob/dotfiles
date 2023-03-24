0ee70ca02b0c687b0bd5ebb06f3a18d2974b9bbf
conjLob <53427774+conjLob@users.noreply.github.com>
2023-03-24 12:26:35 +0900

    Add keyhac and HHKB settings

---
+++ win/keyhac/config.py
@@ -0,0 +1,25 @@
+import pyauto
+from keyhac import *
+
+
+def configure(keymap):
+    keymap.editor = "code"
+    keymap.setFont("HackGen Console NFJ", 16)
+    keymap.setTheme("black")
+    keymap.clipboard_history.enableHook(False)
+
+    keymap_global = keymap.defineWindowKeymap()
+
+    def escAndImeOff():
+        keymap.getWindow().setImeStatus(0)
+        keymap.InputKeyCommand("Esc")()
+
+    keymap_global["Esc"] = escAndImeOff
+    keymap_global["BackSlash"] = "S-BackSlash"
+    keymap_global["A-Yen"] = keymap.InputTextCommand("¥")
+
+    # DeepL
+    keymap_global["W-Q"] = "W-F9"  # translate with popup
+    keymap_global["A-W-Q"] = "A-W-F9"  # translate with window
+    keymap_global["S-W-Q"] = "S-W-F9"  # capture and translate
+    keymap_global["C-W-Q"] = "C-W-F9"  # replace with translation
