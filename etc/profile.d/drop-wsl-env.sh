b91047e0d057d9929994d36f6e76679e44dbe2e2
conjLob <53427774+conjLob@users.noreply.github.com>
2023-11-15 21:47:02 +0900

    Add locale.conf

--- etc/profile.d/drop-wsl-env.sh
+++ etc/profile.d/drop-wsl-env.sh
@@ -1,2 +1,5 @@
 # Drop unnecessary paths added by WSL
 export PATH="$(echo $PATH | sed 's|:/usr/sbin||; s|:/sbin:/bin:/usr/games:/usr/local/games||')"
+
+# Unset WSL locale settings to respect /etc/locale.conf
+unset LANG
